59d4d22f93fc7d2d206c26cf834043127f9bb717
nagasai <nagasairayapudi2000@gmail.com>
2023-09-29 07:50:30 +0000

    first commit

--- src/components/CryptocurrenciesList/index.js
+++ src/components/CryptocurrenciesList/index.js
@@ -1,78 +1,33 @@
-import Loader from 'react-loader-spinner'
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
-
-import {Component} from 'react'
+import './index.css'
 
 import CryptocurrencyItem from '../CryptocurrencyItem'
 
-import './index.css'
-
-class CryptocurrenciesList extends Component {
-  state = {
-    isLoading: true,
-    cryptocurrenciList: [],
-  }
-
-  componentDidMount() {
-    this.getCryptocurrenciData()
-  }
-
-  getCryptocurrenciData = async () => {
-    const response = await fetch(
-      'https://apis.ccbp.in/crypto-currency-converter',
-    )
-    console.log(response.status)
-    const data = await response.json()
-    const updatedData = data.map(eachItem => ({
-      currencyName: eachItem.currency_name,
-      usdValue: eachItem.usd_value,
-      euroValue: eachItem.euro_value,
-      id: eachItem.id,
-      currencyLogo: eachItem.currency_logo,
-    }))
-    this.setState({cryptocurrenciList: updatedData, isLoading: false})
-  }
-
-  render() {
-    const {cryptocurrenciList, isLoading} = this.state
-    return (
-      <div className="app-container">
-        {isLoading ? (
-          <div data-testid="loader">
-            <Loader type="Rings" color="#ffffff" height={80} width={80} />
+const CryptocurrenciesList = props => {
+  const {currenciesData} = props
+  return (
+    <div className="cryptocurrencies-container">
+      <h1 className="main-heading">Cryptocurrency Tracker</h1>
+      <img
+        className="cryptocurrency-image"
+        src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
+        alt="cryptocurrency"
+      />
+      <div className="cryptocurrency-table">
+        <div className="cryptocurrency-info">
+          <p className="coin-type">Coin Type</p>
+          <div className="currency-info">
+            <p className="currency-type">USD</p>
+            <p className="currency-type">EURO</p>
           </div>
-        ) : (
-          <>
-            <h1 className="main-heading">Cryptocurrency Tracker</h1>
-            <div className="items-container">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
-                alt="cryptocurrency"
-                className="currency-img"
-              />
-              <div className="coins-container">
-                <div className="coins-names-container">
-                  <p className="coin-type">Coin Type</p>
-                  <div className="usd-and-euro">
-                    <p className="coin-info">USD</p>
-                    <p className="coin-info">EURO</p>
-                  </div>
-                </div>
-                <ul className="crypto-currency-items-container">
-                  {cryptocurrenciList.map(eachItem => (
-                    <CryptocurrencyItem
-                      currencyData={eachItem}
-                      key={eachItem.id}
-                    />
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </>
-        )}
+        </div>
+        <ul className="cryptocurrencies-items-container">
+          {currenciesData.map(eachItem => (
+            <CryptocurrencyItem key={eachItem.id} currencyData={eachItem} />
+          ))}
+        </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default CryptocurrenciesList
